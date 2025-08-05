868e4067714718868870652294b30f8a9c4f27b4
Ionut321512 <ionut38624@gmail.com>
2025-08-05 17:30:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
-const { Client } = require("discord.js-selfbot-v13");
-const client = new Client();
+const Eris = require("eris");
 
-const token = process.env.DISCORD_TOKEN; // tokenul din Environment Variables
-const spamChannelId = "1086694868132311152";
-const logChannelId = "1393724819216007310";
+const token = process.env.BOT_TOKEN; // Tokenul vine din Environment Variables
+const spamChannelId = "ID_CANAL_SPAM";
+const logChannelId = "ID_CANAL_LOG";
 
 const respondedUsers = new Set();
 
-client.on("ready", () => {
-  console.log(`✅ Sunt logat ca ${client.user.tag}`);
-  setInterval(() => {
-    const spamChannel = client.channels.cache.get(spamChannelId);
-    if (spamChannel) {
-      spamChannel.send("# dm me for 50 skelys").catch(console.error);
-    } else {
-      console.log("❌ Canalul de spam nu a fost găsit!");
-    }
-  }, 126000);
+const bot = new Eris(token, {
+    autoreconnect: true
 });
 
-client.on("messageCreate", (message) => {
-  if (message.channel.type === "DM" && message.author.id !== client.user.id) {
-    if (!respondedUsers.has(message.author.id)) {
-      respondedUsers.add(message.author.id);
+bot.on("ready", () => {
+    console.log(`✅ Logat ca ${bot.user.username}#${bot.user.discriminator}`);
 
-      const logChannel = client.channels.cache.get(logChannelId);
-      if (logChannel) {
-        logChannel
-          .send(`📨 ${message.author.tag} ți-a dat DM!`)
-          .catch(console.error);
-      }
-
-      setTimeout(() => {
-        message.channel.send(
-          "hi u was first create a ticket to claim : https://discord.gg/aGfP2gXm"
-        );
-      }, 9000);
-    }
-  }
+    setInterval(() => {
+        const spamChannel = bot.getChannel(spamChannelId);
+        if (spamChannel) {
+            spamChannel.createMessage("# dm me for 50 skelys")
+                .catch(console.error);
+        } else {
+            console.log("❌ Canalul de spam nu a fost găsit!");
+        }
+    }, 126000); // 126 secunde
 });
 
-client.login(token);
+bot.on("messageCreate", async (message) => {
+    if (message.channel.type === 1 && message.author.id !== bot.user.id) {
+        // DM = type 1 în Eris
+        if (!respondedUsers.has(message.author.id)) {
+            respondedUsers.add(message.author.id);
+
+            const logChannel = bot.getChannel(logChannelId);
+            if (logChannel) {
+                logChannel.createMessage(`📨 ${message.author.username}#${message.author.discriminator} ți-a dat DM!`)
+                    .catch(console.error);
+            }
+
+            setTimeout(() => {
+                message.channel.createMessage(
+                    "hi u was first create a ticket to claim : https://discord.gg/aGfP2gXm"
+                ).catch(console.error);
+            }, 9000);
+        }
+    }
+});
+
+bot.connect();
+
